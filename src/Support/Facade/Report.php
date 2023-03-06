58da1b27d467b7eb0f9b11d78e120de27a281108
steph bug <chronhubgit@gmail.com>
2023-03-06 14:57:07 +0100

    style: minor

--- src/Support/Facade/Report.php
+++ src/Support/Facade/Report.php
@@ -9,9 +9,9 @@ use Chronhub\Storm\Contracts\Reporter\Reporter;
 
 /**
  * @method static Reporter create(string $type, string $name)
- * @method static Reporter command(string $name = 'default')
- * @method static Reporter event(string $name = 'default')
- * @method static Reporter query(string $name = 'default')
+ * @method static Reporter command(string $name='default')
+ * @method static Reporter event(string $name='default')
+ * @method static Reporter query(string $name='default')
  */
 final class Report extends Facade
 {

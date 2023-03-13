4f9324dfe30e323eedcd62a4f41a6bfdc16fbd90
steph bug <chronhubgit@gmail.com>
2023-03-13 18:56:18 +0100

    refactor(projector): move update projection status to trait

--- src/Projection/ConnectionProjectorManager.php
+++ src/Projection/ConnectionProjectorManager.php
@@ -4,24 +4,26 @@ declare(strict_types=1);
 
 namespace Chronhub\Larastorm\Projection;
 
-use Throwable;
-use Illuminate\Database\QueryException;
 use Illuminate\Contracts\Events\Dispatcher;
 use Chronhub\Storm\Projector\Scheme\Context;
 use Chronhub\Storm\Contracts\Projector\Store;
-use Chronhub\Storm\Projector\ProjectionStatus;
 use Chronhub\Storm\Contracts\Projector\ReadModel;
 use Chronhub\Storm\Projector\AbstractProjectorManager;
 use Chronhub\Storm\Projector\Repository\StandaloneStore;
 use Chronhub\Storm\Contracts\Projector\ProjectorRepository;
-use Chronhub\Storm\Projector\Exceptions\ProjectionNotFound;
-use Chronhub\Larastorm\Exceptions\ConnectionProjectionFailed;
 use Chronhub\Storm\Projector\Repository\ReadModelProjectorRepository;
 use Chronhub\Storm\Projector\Repository\PersistentProjectorRepository;
 
 final class ConnectionProjectorManager extends AbstractProjectorManager
 {
+    use InteractWithProjectionProvider;
+
     private ?Dispatcher $eventDispatcher = null;
+
+    public function setEventDispatcher(Dispatcher $eventDispatcher): void
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
 
     protected function createStore(Context $context, string $streamName): Store
     {
@@ -49,60 +51,5 @@ final class ConnectionProjectorManager extends AbstractProjectorManager
         }
 
         return new PersistentProjectorRepository($context, $store, $this->chronicler);
-    }
-
-    /**
-     * @throws ConnectionProjectionFailed
-     */
-    public function stop(string $streamName): void
-    {
-        $this->updateProjectionStatus($streamName, ProjectionStatus::STOPPING);
-    }
-
-    /**
-     * @throws ConnectionProjectionFailed
-     */
-    public function reset(string $streamName): void
-    {
-        $this->updateProjectionStatus($streamName, ProjectionStatus::RESETTING);
-    }
-
-    /**
-     * @throws ConnectionProjectionFailed
-     */
-    public function delete(string $streamName, bool $withEmittedEvents): void
-    {
-        $deleteProjectionStatus = $withEmittedEvents
-            ? ProjectionStatus::DELETING_WITH_EMITTED_EVENTS
-            : ProjectionStatus::DELETING;
-
-        $this->updateProjectionStatus($streamName, $deleteProjectionStatus);
-    }
-
-    public function setEventDispatcher(Dispatcher $eventDispatcher): void
-    {
-        $this->eventDispatcher = $eventDispatcher;
-    }
-
-    /**
-     * @throws ConnectionProjectionFailed
-     * @throws ProjectionNotFound
-     */
-    protected function updateProjectionStatus(string $streamName, ProjectionStatus $projectionStatus): void
-    {
-        try {
-            $success = $this->projectionProvider->updateProjection(
-                $streamName,
-                ['status' => $projectionStatus->value]
-            );
-        } catch (QueryException $exception) {
-            throw ConnectionProjectionFailed::fromQueryException($exception);
-        } catch (Throwable $exception) {
-            throw ConnectionProjectionFailed::failedOnUpdateStatus($streamName, $projectionStatus, $exception);
-        }
-
-        if (! $success) {
-            $this->assertProjectionExists($streamName);
-        }
     }
 }

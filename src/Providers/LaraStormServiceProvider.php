d75bcf1b832f659406573ec1128423c13e87b1d7
steph bug <chronhubgit@gmail.com>
2023-03-13 17:24:38 +0100

    fix:invalid dir

--- src/Providers/LaraStormServiceProvider.php
+++ src/Providers/LaraStormServiceProvider.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Chronhub\Larastorm\Tests\Functional\Providers;
+namespace Chronhub\Larastorm\Providers;
 
 use Illuminate\Support\AggregateServiceProvider;
-use Chronhub\Larastorm\Providers\CqrsServiceProvider;
-use Chronhub\Larastorm\Providers\MessagerServiceProvider;
-use Chronhub\Larastorm\Providers\ProjectorServiceProvider;
-use Chronhub\Larastorm\Providers\AggregateRepositoryServiceProvider;
 
 class LaraStormServiceProvider extends AggregateServiceProvider
 {

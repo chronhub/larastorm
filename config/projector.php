2f83710cf46a070579795dfe90308f84ec849ea3
steph bug <chronhubgit@gmail.com>
2023-03-14 14:11:57 +0100

    docs: comments

--- config/projector.php
+++ config/projector.php
@@ -15,16 +15,21 @@ return [
     | Projection Providers
     |--------------------------------------------------------------------------
     |
-    | Eloquent: use default projection model as projection provider
-    |           connection: database connection name (laravel config)
-    |                       should match with chronicler connection
+    | connection : use db connection to store projection
     |
-    | passing a service id instead of array will be resolved from ioc
+    |   if you only used one projection provider across all projectors,
+    |   you should bind it and pass your service id to the connection.
+    |   note: if you change the table name, you should also change the migration and
+    |   disable the migration below
+    |
+    | in_memory : use in memory projection
+    |
     */
 
     'providers' => [
-        'eloquent' => [
-            'connection' => 'pgsql',
+        'connection' => [
+            'name' => 'pgsql',
+            'table' => 'projections',
         ],
         'in_memory' => \Chronhub\Storm\Projector\Provider\InMemoryProjectionProvider::class,
     ],
@@ -36,11 +41,11 @@ return [
     |
     | Each projector is bound to an event store
     |
-    |       chronicler:     chronicler configuration keys or service registered in ioc
-    |       dispatcher:     dispatch laravel events when projection status changed
-    |       options:        options key
-    |       provider:       projection provider key
-    |       scope:          projection query scope
+    |   chronicler:     chronicler configuration keys or better, an event store service registered in ioc
+    |   dispatcher:     dispatch laravel events when projection status changed
+    |   options:        options key
+    |   provider:       projection provider key
+    |   scope:          projection query scope
     */
 
     'projectors' => [
@@ -51,7 +56,7 @@ return [
                 'chronicler' => ['connection', 'publish'],
                 'dispatcher' => true,
                 'options' => 'default',
-                'provider' => 'eloquent',
+                'provider' => 'connection',
                 'scope' => \Chronhub\Larastorm\Projection\ConnectionQueryScope::class,
             ],
 
@@ -59,7 +64,7 @@ return [
                 'chronicler' => ['connection', 'read'],
                 'dispatcher' => true,
                 'options' => 'default',
-                'provider' => 'eloquent',
+                'provider' => 'connection',
                 'scope' => \Chronhub\Larastorm\Projection\ConnectionQueryScope::class,
             ],
         ],
@@ -80,9 +85,23 @@ return [
     |--------------------------------------------------------------------------
     |
     | Options can be an array or a string service class/id implementing projector option contract
-    | not that a class or service id is immutable
+    | not that a class or service id is immutable and, you won't be able to change it when running
+    | a projection, for example, from a console command.
     |
+    | note that array will be merged into a default projector option instance
     | @see \Chronhub\Storm\Projector\Options\DefaultProjectorOption
+    |
+    |   Signal              :  dispatch async signal
+    |   Lockout             :  lock threshold incrementation in milliseconds
+    |   Timeout             :  lock timeout in milliseconds
+    |   Sleep               :  time in milliseconds to sleep before updating lock
+    |   Block size          :  number of stream events to be persisted before ending a cycle
+    |   Retries             :  an array of retries in milliseconds or a string of retries separated by comma,
+    |                          representing arguments of the range function
+    |   Detection windows   :  a valid string interval, for example, 'PT1H' or 'PT1M'
+    |                          Use when resetting projection to avoid gap detection which past the detection windows,
+    |                          compared to stream event time and present time
+    |                          note that an empty retries options will make this option useless
     */
 
     'options' => [

9bd0bcb4a0ca40e54f5b41b1bf6265426456a56c
steph bug <chronhubgit@gmail.com>
2023-04-18 10:49:31 +0200

    style

--- database/event_stream/2022_06_18_000000_create_event_streams_table.php
+++ database/event_stream/2022_06_18_000000_create_event_streams_table.php
@@ -11,13 +11,15 @@ return new class() extends Migration
 {
     public function up(): void
     {
-        Schema::create(EventStreamProvider::TABLE_NAME, static function (Blueprint $table): void {
-            $table->bigInteger('id', true);
-            $table->string('real_stream_name', 150)->unique();
-            $table->string('stream_name', 150);
-            $table->string('category', 60)->nullable();
+        Schema::create(
+            EventStreamProvider::TABLE_NAME,
+            static function (Blueprint $table): void {
+                $table->bigInteger('id', true);
+                $table->string('real_stream_name', 150)->unique();
+                $table->string('stream_name', 150);
+                $table->string('category', 60)->nullable();
 
-            $table->index('category');
+                $table->index('category');
         });
     }
 
--- database/projection/2022_06_18_000000_create_projections_table.php
+++ database/projection/2022_06_18_000000_create_projections_table.php
@@ -11,13 +11,15 @@ return new class() extends Migration
 {
     public function up(): void
     {
-        Schema::create(ConnectionProvider::TABLE_NAME, static function (Blueprint $table): void {
-            $table->bigInteger('no', true);
-            $table->string('name', 150)->unique();
-            $table->json('position');
-            $table->json('state');
-            $table->string('status', 28);
-            $table->char('locked_until', 26)->nullable();
+        Schema::create(
+            ConnectionProvider::TABLE_NAME,
+            static function (Blueprint $table): void {
+                $table->bigInteger('no', true);
+                $table->string('name', 150)->unique();
+                $table->json('position');
+                $table->json('state');
+                $table->string('status', 28);
+                $table->char('locked_until', 26)->nullable();
         });
     }
 

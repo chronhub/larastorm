a1ef2476790be2f7040439b44d202886e9746846
steph bug <chronhubgit@gmail.com>
2023-05-05 09:23:09 +0200

    fix stream event loader

--- src/EventStore/Loader/EventLoader.php
+++ src/EventStore/Loader/EventLoader.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Chronhub\Larastorm\EventStore\Loader;
 
-use Chronhub\Larastorm\Exceptions\ConnectionQueryFailure;
 use Chronhub\Storm\Chronicler\Exceptions\NoStreamEventReturn;
 use Chronhub\Storm\Chronicler\Exceptions\StreamNotFound;
 use Chronhub\Storm\Contracts\Chronicler\StreamEventLoader;
 use Chronhub\Storm\Contracts\Serializer\StreamEventSerializer;
 use Chronhub\Storm\Stream\StreamName;
 use Generator;
+use Illuminate\Database\QueryException;
 use stdClass;
 
 final readonly class EventLoader implements StreamEventLoader
@@ -21,26 +21,31 @@ final readonly class EventLoader implements StreamEventLoader
 
     /**
      * @throws StreamNotFound
-     * @throws ConnectionQueryFailure
      */
     public function __invoke(iterable $streamEvents, StreamName $streamName): Generator
     {
-        $count = 0;
+        try {
+            $count = 0;
 
-        foreach ($streamEvents as $streamEvent) {
-            if ($streamEvent instanceof stdClass) {
-                $streamEvent = (array) $streamEvent;
+            foreach ($streamEvents as $streamEvent) {
+                if ($streamEvent instanceof stdClass) {
+                    $streamEvent = (array) $streamEvent;
+                }
+
+                yield $this->serializer->deserializePayload($streamEvent);
+
+                $count++;
             }
 
-            yield $this->serializer->deserializePayload($streamEvent);
+            if ($count === 0) {
+                throw NoStreamEventReturn::withStreamName($streamName);
+            }
 
-            $count++;
+            return $count;
+        } catch (QueryException $exception) {
+            if ($exception->getCode() !== '00000') {
+                throw StreamNotFound::withStreamName($streamName);
+            }
         }
-
-        if ($count === 0) {
-            throw NoStreamEventReturn::withStreamName($streamName);
-        }
-
-        return $count;
     }
 }
